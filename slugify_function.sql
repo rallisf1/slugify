b6f5946ce8603852e4af148a82bf4c5c0a8872ef
John Rallis <rallisf1@yahoo.gr>
2021-08-04 00:35:22 +0300

    greek customization
    edit character table for greek and minor code changes

--- slugify_function.sql
+++ slugify_function.sql
@@ -1,5 +1,6 @@
-﻿DROP FUNCTION IF EXISTS `SLUGIFY`;
-DELIMITER ;;
+DROP FUNCTION IF EXISTS `SLUGIFY`;
+
+DELIMITER //
 
 CREATE FUNCTION `SLUGIFY`(dirty_string VARCHAR(255))
 	RETURNS VARCHAR(255)
@@ -13,7 +14,7 @@ BEGIN
 	
 	SET new_string = '';
     -- Replace all spaces and dots with a dash
-	SET dirty_string = REPLACE(TRIM(REPLACE(dirty_string, '.', '-')), ' ', '-');
+	SET dirty_string = LOWER(REPLACE(TRIM(REPLACE(dirty_string, '.', '-')), ' ', '-'));
     -- Set the allowed characters to be any letter from the english alphabet, numbers or hyphens
 	SET allowed_chars = 'abcdefghijklmnopqrstuvwxyz0123456789-';
 	SET string_length = CHAR_LENGTH(dirty_string);
@@ -27,7 +28,7 @@ BEGIN
 	IF temp_table_rows = 0 THEN
 		INSERT INTO slug_temp_table (bul_letter, eng_equiv) VALUES 
             -- Replace the following line with the your preferred character map
-            ('а', 'a'), ('б', 'b'), ('в', 'v'), ('г', 'g'), ('д', 'd'), ('е', 'e'), ('ж', 'j'), ('з', 'z'), ('и', 'i'), ('й', 'j'), ('к', 'k'), ('л', 'l'), ('м', 'm'), ('н', 'n'), ('о', 'o'), ('п', 'p'), ('р', 'r'), ('с', 's'), ('т', 't'), ('у', 'u'), ('ф', 'f'), ('х', 'h'), ('ц', 'c'), ('ч', 'ch'), ('ш', 'sh'), ('щ', 'sht'), ('ъ', 'y'), ('ь', 'i'), ('ю', 'u'), ('я', 'ja')
+            ('α', 'a'), ('β', 'b'), ('γ', 'g'), ('δ', 'd'), ('ε', 'e'), ('ζ', 'z'), ('η', 'h'), ('θ', '8'), ('ι', 'i'), ('κ', 'k'), ('λ', 'l'), ('μ', 'm'), ('ν', 'n'),  ('ξ', '3'), ('ο', 'o'), ('π', 'p'), ('ρ', 'r'), ('σ', 's'), ('τ', 't'), ('υ', 'y'), ('φ', 'f'), ('χ', 'x'), ('ψ', 'ps'), ('ω', 'w')
         ;
 	END IF;
     
@@ -37,7 +38,7 @@ BEGIN
 		SET current_char = SUBSTRING(dirty_string, counter, 1);
 		SET is_allowed = LOCATE(current_char, allowed_chars);
 		IF is_allowed > 0 THEN
-			SET new_string = CONCAT(new_string, LOWER(current_char));
+			SET new_string = CONCAT(new_string, current_char);
 		ELSE
 			SELECT slug_temp_table.eng_equiv INTO found_equiv FROM slug_temp_table WHERE slug_temp_table.bul_letter = current_char;
 			IF CHAR_LENGTH(found_equiv) > 0 THEN
@@ -57,5 +58,7 @@ BEGIN
     
 	RETURN new_string;
 
-END;;
+END
+//
+
 DELIMITER ;
